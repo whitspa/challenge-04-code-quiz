5739ad6cfdb6b327244e15257e9f7f61b71e0d80
whit <whitspa64@gmail.com>
2022-10-23 15:27:04 -0400

    third commit Outlined the Quiz structore in HTML and Drafted some of the javascript components

---
+++ script.js
@@ -0,0 +1,11 @@
+//preliminary thinking:..
+const span = document.getElementById("question");
+//const question = Array of objects
+//loop through it with a user keydown and a fcn to verify user answer something like:
+document.addEventListener["keydown",function(tbd){
+    
+    console.log();
+    tbd.key === question[i];
+    hiddenAnswer[i] =tbd.key;
+    question.textContent = hiddenAnswer;
+}];
