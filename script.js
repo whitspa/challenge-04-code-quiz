071e2b488ce23bff0b1d4803320f6abd6ba9d42f
whit <whitspa64@gmail.com>
2022-10-25 17:14:17 -0400

    Worked on timer and counter functions plus reorganized javascript file to follow function order.

--- script.js
+++ script.js
@@ -8,6 +8,15 @@
     //hiddenAnswer[i] =tbd.key;
     //question.textContent = hiddenAnswer;
     //}];
+
+    //Issues:
+    //Get scoring working
+    //Enter and Save initials and score
+    //Display score?
+    //Format answer buttons stacked to the left
+    //How to get button color to more closely match mock up
+
+
     
   let questions = [
   { question: "Commonly used data types DO NOT include?", answers: ["strings", "booleans", "alerts", "numbers"], correct: "alerts" },
@@ -21,16 +30,45 @@
   { question: "A very useful tool used during development and debugging for printing content to the debugger is", answers: ["javascript", "terminal bash", "for loops", "console log"], correct: "console log" },
 ];
 
-
-var questionLog = 0;
+//Here are the variables we need to keep track of. I'm thinking keep them in an object called playerData
+var questionLog = 0; //records the user's answer choice for each question
 var playerScore = 0;
-var timer,
-counter = 60;
-//Need to flesh out the timer fcn and scoring fcn
+var timer;
+var counter = 60;
+var playerInitials ="";
+var highscorers = [];//not sure yet about this
+var playerData = {};
+var quizResultData = {}; //For storing multiple quiz scores
+var endOfQuiz = 0; //when no time is left
+
+//end quiz function and enter user initials
+function startQuiz() {
+  var quizScreen = document.getElementById("quizSection");
+     var quizInstructions = document.getElementById("quizDirections");
+     var timerTime = document.getElementById("timerText");
+         timerTime.classList.remove("invisible");
+         quizInstructions.classList.add("invisible");
+         quizScreen.classList.remove("invisible");
+         quizScreen.classList.add("startScreen");
+       displayQuestion();
+     }
+function countdown(){
+  document.getElementById("counter").innerHTML = counter;
+  timer = setInterval(function (){counter--;
+  document.getElementById("counter").innerHTML = counter;
+if (counter === 0 && endOfQuiz === 0){
+  clearInterval(timer);
+  endQuiz();
+   }
+  }, 1000);
+}
+
+
+
 
 function displayQuestion() { //displays each question and the four multiple choice options
   document.getElementById("question").innerHTML =
-    questions[questionLog].question;
+  questions[questionLog].question;
   document.getElementById("answer-1").innerHTML =
     questions[questionLog].answers[0];
   document.getElementById("answer-2").innerHTML =
@@ -42,21 +80,13 @@ function displayQuestion() { //displays each question and the four multiple choi
 }
 
 //click to start quiz
-document.getElementById("startButton").addEventListener("click",startQuiz);
+// document.getElementById("startButton").addEventListener("click",resetTimer);
 //need to add start counter
 
 //Need a fcn to list scores and an object to store game results/scores
- 
-  //function to start quiz. need to add start counter
-function startQuiz() {
-   var quizScreen = document.getElementById("quizSection");
-   var quizInstructions = document.getElementById("quizDirections");
-       quizInstructions.classList.add("invisible");
-       quizScreen.classList.remove("invisible");
-       quizScreen.classList.add("startScreen");
-     displayQuestion();
-   }
- 
+
+//function to start quiz. need to add start counter
+
  //validating answers to questions as either "correct" or "wrong".
  //Still working on the 10 second penalty for wrong answers.
   document.getElementById("answer-1").addEventListener("click", function () {
@@ -86,7 +116,7 @@ function startQuiz() {
       document.getElementById("result").innerHTML ="Correct!"
     } else {
       console.log("wrong");
-    
+      
       document.getElementById("result").innerHTML ="Wrong!"
       counter = counter - 10;
     }
@@ -110,7 +140,7 @@ function startQuiz() {
       console.log("Wrong!");
       
       document.getElementById("result").innerHTML ="Wrong!"
-  
+      
       counter = counter - 10;
     }
     questionLog++;
@@ -126,13 +156,13 @@ function startQuiz() {
   document.getElementById("answer-4").addEventListener("click", function () {
     if (this.textContent === questions[questionLog].correct) {
       console.log("correct");
-    
+      
       document.getElementById("result").innerHTML ="Correct!"
     } else {
       console.log("wrong");
-     
+      
       document.getElementById("result").innerHTML ="Wrong!"
-  
+      
       counter = counter - 10;
     }
     questionLog++;
@@ -142,6 +172,23 @@ function startQuiz() {
       displayQuestion();
     }
   });
-
+  
+  function endQuiz() {
+    alert("Ended")
+    return
+    var quizscreen =document.getElementById("quizSection");
+    var quizInstructions = document.getElementById("quizDirections");
+  var timeRemaining = document.getElementById("timerText");
+      endOfQuiz = 1;
+      playerScore = counter;
+      document.getElementsByClassName("countDown");
+      quizInstructions.classList.add("invisible");
+      quizScreen.classList.add("invisible");
+      document.getElementById("result").innerHTML = ("All Done! Your final score is "+ playerScore);
+      timeRemaining.classList.add("invisible");
+    }
     
-      
+
+    document.getElementById("startButton").addEventListener("click",startQuiz);
+    document.getElementById("startButton").addEventListener("click",countdown);
+  
